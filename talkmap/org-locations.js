830b5ec520850a2f1cc9386751ca59ba67b8683d
marisol <marisolosman17@gmail.com>
2024-01-01 19:43:53 +0100

    December 2023 updates

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,13 +1,13 @@
 var addressPoints = [
   [
     "Boulder, Colorado, USA",
-    40.0154155,
-    -105.270241
+    40.0149856,
+    -105.270545
   ],
   [
     "Orono, Main, USA",
-    44.882707249999996,
-    -68.67316299623619
+    44.8801501,
+    -68.6747908
   ],
   [
     "Mendoza, Argentina",
@@ -15,14 +15,34 @@ var addressPoints = [
     -68.43818677312292
   ],
   [
+    "Virtual",
+    57.9110833,
+    59.9823317
+  ],
+  [
+    "Reading, United Kingdom",
+    51.4564242,
+    -0.9700664
+  ],
+  [
     "Sydney, Australia",
     -33.8698439,
     151.2082848
   ],
   [
+    "Vienna, Austria",
+    48.2083537,
+    16.3725042
+  ],
+  [
     "Bonn, Germany",
     50.735851,
     7.10066
+  ],
+  [
+    "Kigali, Rwanda",
+    -1.950851,
+    30.061507
   ],
   [
     "Santiago de Chile, Chile",
